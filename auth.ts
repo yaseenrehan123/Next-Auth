95d788deb1e1b2d681f324b5c30b57910c5932e2
MagstarDev <yr6190457@gmail.com>
2026-02-10 18:52:38 +0500

    Credentials Implemented

--- auth.ts
+++ auth.ts
@@ -1,5 +1,39 @@
 import NextAuth from "next-auth"
-
+import Credentials from "next-auth/providers/credentials";
+import prisma from "./lib/prisma";
+import { compare } from "bcrypt";
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [],
+    providers: [
+        Credentials({
+
+            authorize: async (credentials) => {
+                const email: string = credentials?.email as string || "";
+                const password: string = credentials?.password as string || "";
+
+                const user = await prisma.user.findUnique({
+                    where: { email: email }
+                });
+
+                if (!user) {
+                    return null
+                    //throw new Error(`NO USER FOUND! ${email}`);
+                };
+
+                const isCorrectPassword: boolean = await compare(password, user.hashedPassword);
+
+                if (!isCorrectPassword) {
+                    return null
+                    //throw new Error("PASSWORD NOT CORRECT!");
+                };
+
+                return user;
+            }
+        })
+    ],
+    session: {
+        strategy: "jwt"
+    },
+    pages: {
+        signIn: "/login"
+    }
 })
